5e13d2eb9e159eba69fec94d4534baf75930ed7a
rajeevshivah <rajeevshivah@gmail.com>
2023-05-02 03:40:28 -0700

    Header done

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../Header";
+
+const IndexPage = () => {
+  return <div>Index Page here</div>;
+};
+
+export default IndexPage;
