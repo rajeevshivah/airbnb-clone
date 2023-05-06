57c9e7731fc1731a1d68837dc002f6ae735ca5d6
rajeevshivah <raajeev.ranjann@gmail.com>
2023-05-06 22:33:33 +0530

    api initiated & database connected

--- api/index.js
+++ api/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
-
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
 const app = express();
-
+const User = require("./Models/User.js");
+app.use(express.json());
+const bcryptSalt = bcrypt.genSaltSync(10);
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userDoc = await User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
+  res.json(userDoc);
+});
+
 app.listen(4000);
+
+//Alj8DxZoCCsuqfnD
